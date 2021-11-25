af4cac7a22512a2a26976f66c282750e567ae779
Radim Daniel Panek <rdpanek@gmail.cz>
2021-11-25 16:03:42 +0100

    wdio/5-the-internet-login-pt-audit.js

---
+++ wdio/5-the-internet-login-pt-audit.js
@@ -0,0 +1,27 @@
+describe('More steps and login', () => {
+  it('open page', async () => {
+    await browser.url('/login')
+  })
+
+  it('performance audit', async () => { await browser.performAudit(await browser.getUrl()) })
+
+  it('fill login form', async () => {
+    const userElm = await $('#username')
+    await userElm.setValue(browser.config.username)
+
+    const passElm = await $('#password')
+    await passElm.setValue(browser.config.password)
+  })
+
+  it('login user', async () => {
+    const buttonElm = await $('button[type="submit"]')
+    buttonElm.click()
+
+    await expect($('#flash')).toBeExisting()
+    await expect($('#flash')).toHaveTextContaining(
+        'You logged into a secure area!')
+  })
+
+  it('performance audit', async () => { await browser.performAudit(await browser.getUrl()) })
+})
+
