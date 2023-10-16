5c1ec4685028cbc208e0ab6cb1bff845fd411d3f
Radim Daniel Pánek <rdpanek@gmail.com>
2023-10-16 15:21:08 +0200

    Create service-worker.js

---
+++ others/service-worker.js
@@ -0,0 +1,19 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+      caches.open('hello-world-cache').then(function(cache) {
+          return cache.addAll([
+              '/',
+              '/index.html',
+              '/service-worker.js'
+          ]);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+      caches.match(event.request).then(function(response) {
+          return response || fetch(event.request);
+      })
+  );
+});
